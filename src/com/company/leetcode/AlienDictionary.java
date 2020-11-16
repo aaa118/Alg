a6aa2160aef69740e43eb1dbe8f7151d442a7f25
aaa118 <aaavs118@gmail.com>
2020-11-16 11:03:17 -0500

    Update

--- src/com/company/leetcode/AlienDictionary.java
+++ src/com/company/leetcode/AlienDictionary.java
@@ -3,10 +3,17 @@ package com.company.leetcode;
 public class AlienDictionary {
 
     public static void main(String[] args) {
+        String[] words = {"word","world","row"};
+        String order = "worldabcefghijkmnpqstuvxyz";
+        String[] words2 = {"hello","leetcode"};
+        String order2 = "hlabcdefgijkmnopqrstuvwxyz";
+
+        System.out.println(isAlienSorted2(words, order));
+        System.out.println(isAlienSorted2(words2, order2));
 
     }
 
-    public static boolean isAlienSorted(String[] words, String order) {
+    public static boolean isAlienSorted1(String[] words, String order) {
         int[] index = new int[26];
         for (int i = 0; i < order.length(); ++i)
             index[order.charAt(i) - 'a'] = i;
@@ -35,20 +42,24 @@ public class AlienDictionary {
         return true;
     }
 
-    public boolean isAlienSorted2(String[] words, String order) {
+    public static boolean isAlienSorted2(String[] words, String order) {
         int len = words.length;
         for (int i = 0; i < len - 1; i++) {
             int worlen1 = words[i].length();
             int worlen2 = words[i + 1].length();
-            int worlen = worlen1 > worlen2 ? worlen2 : worlen1;
+            int worlen = Math.min(worlen1, worlen2);
             for (int j = 0; j < worlen; j++) {
                 int curr = order.indexOf(words[i].charAt(j));
                 int next = order.indexOf(words[i + 1].charAt(j));
                 if (curr < next) break;
-                else if (curr == next) continue;
-                else if (curr > next) return false;
+                else if (curr == next) {
+                    // Do Nothing
+
+                }
+                else return false;
             }
-            if (order.indexOf(words[i].charAt(worlen - 1)) == order.indexOf(words[i + 1].charAt(worlen - 1)) && worlen1 > worlen2)
+            if (order.indexOf(words[i].charAt(worlen - 1)) == order.indexOf(words[i + 1]
+                    .charAt(worlen - 1)) && worlen1 > worlen2)
                 return false;
         }
 
