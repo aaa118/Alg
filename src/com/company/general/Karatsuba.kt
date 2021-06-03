4d5e8578a3420df7b83c3fc03a69013f4ef32493
aaa118 <aaavs118@gmail.com>
2021-06-02 20:04:28 -0400

    Fixed Karatsuba

--- src/com/company/general/Karatsuba.kt
+++ src/com/company/general/Karatsuba.kt
@@ -1,37 +1,33 @@
 package com.company.general
 
-import com.company.hackerRank.kotlin.DivisibleSumPairs
 import kotlin.math.abs
 import kotlin.math.log10
 import kotlin.math.pow
 
 class Karatsuba {
-
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-//            val arrays = arrayOf(1, 3, 2, 6, 1, 2)
-////            val d = 3
-
-////            val m = 2
-//            val k = 3
-//            val n = 6
-//            println(DivisibleSumPairs.divisibleSumPairs(n, k, arrays))
-
-            karatsubaMulti(5678, 1234)
+            println(karatsubaMulti(5678, 1234))
         }
 
-        fun karatsubaMulti(number1: Int, number2: Int) {
-            val num1Length = number1.length()
-            val num2Length = number2.length()
-            println(num1Length)
-            println(num2Length)
-            val ans = 5.0.pow(2.0).toInt()
-
+        private fun karatsubaMulti(number1: Int, number2: Int): Int {
+            val num1HalfLength = number1.length() / 2
+            if (num1HalfLength == 1) {
+                return number1 * number2
+            }
+            val num2HalfLength = number2.length()
+            val tenPowerOfHalfLength = 10.0.pow(num1HalfLength).toInt()
+            val tenPowerOfFullLength = 10.0.pow(num2HalfLength).toInt()
+            val a = number1 / tenPowerOfHalfLength
+            val b = number1 % tenPowerOfHalfLength
+            val c = number2 / tenPowerOfHalfLength
+            val d = number2 % tenPowerOfHalfLength
+            return (tenPowerOfFullLength * (a * c)) + (tenPowerOfHalfLength * ((a * d) + (b * c))) + (b * d)
 
         }
 
-        fun Int.length() = when(this) {
+        fun Int.length() = when (this) {
             0 -> 1
             else -> log10(abs(toDouble())).toInt() + 1
         }
